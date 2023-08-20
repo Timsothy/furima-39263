a48cc3f8adb26169ef109a629cc99b1162da2c9f
Timsothy <timothy.ms591@gmail.com>
2023-08-20 13:15:46 +0900

    ユーザー管理機能

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,9 +51,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは半角英数字混合でないと登録できない' do
+      it 'passwordが英字のみでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      it 'passwordが数字のみでは登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
+      it 'passwordが全角文字を含んでいると登録できない' do
+        @user.password = 'abc１２３'
+        @user.password_confirmation = 'abc１２３'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
