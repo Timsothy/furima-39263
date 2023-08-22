b749d9197a95537133ade1225f876bc876f6075e
Timsothy <timothy.ms591@gmail.com>
2023-08-22 10:47:08 +0900

    商品データの保存

---
+++ app/models/item.rb
@@ -0,0 +1,21 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+  belongs_to :user
+  has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は¥300以上¥9,999,999以下の半角数値で入力してください' },
+                    format: { with: /\A[0-9]+\z/, message: '半角数値のみ入力してください' }
+end
