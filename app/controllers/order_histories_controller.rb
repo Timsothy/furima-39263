5c68a637a0d50544311b371a9e0e79d1b5fa3951
Timsothy <timothy.ms591@gmail.com>
2023-09-13 11:25:40 +0900

    PAY.JPの導入

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -7,6 +7,7 @@ class OrderHistoriesController < ApplicationController
   def create
     @order_form = OrderForm.new(order_history_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -20,4 +21,13 @@ class OrderHistoriesController < ApplicationController
   def order_history_params
     params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      card: order_history_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,15 +1,14 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :item, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_history
+  attr_accessor :item, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :order_history, :token
 
   validates :postal_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :city, presence: true
   validates :addresses, presence: true
   validates :phone_number, presence: true,
-                           numericality: { only_integer: true,
-                                           greater_than_or_equal_to: 10, less_than_or_equal_to: 11,
-                                           message: 'is invalid' }
+                           format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+  validates :token, presence: true
 
   def save
     order_history = OrderHistory.create(item: item)
