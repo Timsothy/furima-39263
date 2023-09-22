237adf781dd3fb057ecf1025e7f0cd9668b98239
Timsothy <timothy.ms591@gmail.com>
2023-09-22 21:06:51 +0900

    テストコードの修正

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
     addresses { Faker::Address.street_address }
     building { Faker::Address.building_number }
     phone_number { Faker::Number.number(digits: [10, 11].sample) }
-    item_id { 1 }
-    user_id { 1 }
   end
 end
